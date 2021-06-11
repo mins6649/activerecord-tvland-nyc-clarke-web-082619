4c580280a9526a27ceab6b36ae08226f3e0f7ef8
Minyoung Shin <mins6649@gmail.com>
2021-06-11 17:14:31 -0400

    commit tree 465c7d47bfd3ac03bbcace88381f6c3db421e130

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    fullname = []
+    fullname << self.first_name
+    fullname << self.last_name
+    fullname.join(' ')
+  end
+
+  def list_roles
+    characters.collect do |character|
+        "#{character.name} - #{character.show.name}"
+    end
+  end
 end
